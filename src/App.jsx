5121ecbd0b7ca2e791aa43e7c5f91cdbbfa7c09d
i-mkrishna <134141496+i-mkrishna@users.noreply.github.com>
2025-03-03 11:01:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
