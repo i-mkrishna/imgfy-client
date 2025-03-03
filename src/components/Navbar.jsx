fe93d64d7f2550b3bb5d134dcd04a6b92ef8dbbb
i-mkrishna <134141496+i-mkrishna@users.noreply.github.com>
2025-03-03 10:54:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
-import { AppContext } from "../Context/AppContext.jsx";
+import { AppContext } from "../context/AppContext.jsx";
 
 const Navbar = () => {
   const { user, setShowLogin, logout, credits } = useContext(AppContext);
